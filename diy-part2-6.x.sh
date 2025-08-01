cc620aae9f3be37f4af773fbba632b3bc554c584
519561910@qq.com <519561910@qq.com>
2025-08-01 08:37:03 +0800

    rtl8367b交换机驱动更换成dsa模型

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -34,7 +34,7 @@ echo -e "\\ndefine Device/nsy_g68-plus
   DEVICE_VENDOR := NSY
   DEVICE_MODEL := G68
   DEVICE_DTS := rk3568/rk3568-nsy-g68-plus
-  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7916-firmware
+  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-thermal kmod-dsa-realtek kmod-dsa-tag-rtl4-a kmod-r8169 kmod-mt7916-firmware
 endef
 TARGET_DEVICES += nsy_g68-plus" >> target/linux/rockchip/image/legacy.mk
 
@@ -45,7 +45,7 @@ echo -e "\\ndefine Device/nsy_g16-plus
   DEVICE_VENDOR := NSY
   DEVICE_MODEL := G16
   DEVICE_DTS := rk3568/rk3568-nsy-g16-plus
-  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7615-firmware
+  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-thermal kmod-dsa-realtek kmod-dsa-tag-rtl4-a kmod-r8169 kmod-mt7615-firmware
 endef
 TARGET_DEVICES += nsy_g16-plus" >> target/linux/rockchip/image/legacy.mk
 
@@ -56,31 +56,22 @@ echo -e "\\ndefine Device/bdy_g18-pro
   DEVICE_VENDOR := BDY
   DEVICE_MODEL := G18
   DEVICE_DTS := rk3568/rk3568-bdy-g18-pro
-  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-r8169 kmod-switch-rtl8367b swconfig kmod-swconfig kmod-mt7615-firmware
+  DEVICE_PACKAGES += kmod-nvme kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-thermal kmod-dsa-realtek kmod-dsa-tag-rtl4-a kmod-r8169 kmod-mt7615-firmware
 endef
 TARGET_DEVICES += bdy_g18-pro" >> target/linux/rockchip/image/legacy.mk
 
 
-rm -f target/linux/generic/hack-6.6/744-rtl8366_smi-fix-ce-debugfs.patch
+mkdir -p target/linux/rockchip/files/drivers/net/phy
+cp -f $GITHUB_WORKSPACE/configfiles/rtl8365mb.c target/linux/rockchip/files/drivers/net/phy/rtl8365mb.c
 
 
 rm -f target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 cp -f $GITHUB_WORKSPACE/configfiles/02_network target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 
-# 加入nsy_g68-plus初始化网络配置脚本
-cp -f $GITHUB_WORKSPACE/configfiles/swconfig_install package/base-files/files/etc/init.d/swconfig_install
-chmod 755 package/base-files/files/etc/init.d/swconfig_install
-
-
 # 集成 nsy_g68-plus WiFi驱动
 mkdir -p package/base-files/files/lib/firmware/mediatek
 cp -f $GITHUB_WORKSPACE/configfiles/WirelessDriver/mt7916_eeprom.bin package/base-files/files/lib/firmware/mediatek/mt7916_eeprom.bin
-
-
-# rtl8367b驱动压缩包，暂时使用这样替换
-wget https://github.com/xiaomeng9597/files/releases/download/files/rtl8367b-fix-gmac.tar.gz
-tar -xvf rtl8367b-fix-gmac.tar.gz
 
 
 # 复制dts设备树文件到指定目录下
